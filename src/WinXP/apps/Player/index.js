16c15294e2294c7f8a4617a3ee90161ccda98062
polofesse <paul.jousselin@gmail.com>
2024-06-05 23:00:44 +0200

    resizable video

--- src/WinXP/apps/Player/index.js
+++ src/WinXP/apps/Player/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactPlayer from 'react-player/youtube';
 
-const Player = () => <ReactPlayer url="https://youtu.be/nRNGZWL3jeg" />;
+const Player = () => (
+  <ReactPlayer url="https://youtu.be/nRNGZWL3jeg" width="100%" height="100%" />
+);
 
 export default Player;
--- src/WinXP/apps/Player2/index.js
+++ src/WinXP/apps/Player2/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactPlayer from 'react-player/youtube';
 
-const Player2 = () => <ReactPlayer url="https://youtu.be/Axq2ZKl-7sA" />;
+const Player2 = () => (
+  <ReactPlayer url="https://youtu.be/Axq2ZKl-7sA" width="100%" height="100%" />
+);
 
 export default Player2;
